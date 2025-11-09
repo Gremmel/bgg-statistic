1ac6357d68c07db45c759e6de29cea08dc35bf46
Gremmel <mc@soseies.de>
2025-11-09 13:33:05 +0100

    socketio

--- ui/src/main.js
+++ ui/src/main.js
@@ -18,7 +18,7 @@ const app = createApp(App).use(store).use(router);
 
 console.log('Environment Mode:', import.meta.env.MODE);
 
-io('https://my-statistic.soseies.de');
+const socket = io('https://my-statistic.soseies.de');
 
 // const socket = import.meta.env.MODE === 'development' ?
 //   io('http://localhost:3000') :
